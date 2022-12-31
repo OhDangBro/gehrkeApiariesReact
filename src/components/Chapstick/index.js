b4550fdb98825651668f0e89355a4c0712bf5446
William Gehrke <williamegehrke@gmail.com>
2022-12-31 15:12:15 -0500

    chapstick section restyle, removed honey section

--- src/components/Chapstick/index.js
+++ src/components/Chapstick/index.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
+import Bounce from 'react-reveal/Bounce';
+import ChapstickOne from "../../assets/images/ChapstickImages/Chapstick1.jpg"
+import ChapstickTwo from "../../assets/images/ChapstickImages/Chapstick2.jpg"
 import ChapstickThree from "../../assets/images/ChapstickImages/Chapstick3.jpg"
-
+import ChapstickFour from "../../assets/images/ChapstickImages/Chapstick4.jpg"
+import ChapstickFive from "../../assets/images/ChapstickImages/Chapstick5.jpg"
 import ChapstickIcon from "../../assets/images/ChapstickImages/lip-balm.png"
-
+import Box from '@mui/material/Box';
 
 
 
@@ -13,12 +18,34 @@ import ChapstickIcon from "../../assets/images/ChapstickImages/lip-balm.png"
 
 function Chapstick() {
     return (
+<>
+
 <section id="Chapstick">
 <div className="honeySection">
-   <img alt="chapstick" src={ChapstickThree} class="split chapstickleft" />
+   <div class="split chapstickleft" >
+  
+   <div class = "chapstickgallery">
+  <div class="clipped-border">
+    <img src={ChapstickOne} id="clipped"/>
+  </div>
+  <div class="clipped-border">
+    <img src={ChapstickTwo} id="clipped"/>
+  </div>
+  <div class="clipped-border">
+    <img src={ChapstickThree} id="clipped"/>
+  </div>
+    <div class="clipped-border">
+    <img src={ChapstickFour} id="clipped"/>
+  </div>
+    <div class="clipped-border">
+    <img src={ChapstickFive} id="clipped"/>
+  </div>
+  <div class = "shadow"></div>
+</div>
+   </div>
 <div class="split chapstickright">
  <div class="centered">
- <img className="honeyIcon" alt="chapstick icon" src={ChapstickIcon}/>
+ <img className="chapstickIcon" alt="chapstick icon" src={ChapstickIcon}/>
    <h2 className="chapstickText">Chapstick</h2>
    <p className="chapstickP">When people find out that we are honey bee farming, they always ask about the honey. But bees also produce beeswax and something will need to be done with the beeswax when you harvest honey. We take this beeswax and filter out the impurities using a simple method of boiling in water and filtering. From there we just mix the beeswax with 1 parts beeswax and 2 parts coconut oil and a few drops of vitamin e. All the ingredients are organic.</p>
  </div>
@@ -26,7 +53,16 @@ function Chapstick() {
 </div>
        
 </section>
+      
+
+
+
+       </>
     );
   }
   
   export default Chapstick;
+
+
+
+ 
