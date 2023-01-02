14f1e8b76d1da12e328dc8ec9c8621bf2ee75de2
William Gehrke <williamegehrke@gmail.com>
2023-01-02 14:10:27 -0500

    chapstick section styling for mobile

--- src/components/Chapstick/index.js
+++ src/components/Chapstick/index.js
@@ -16,50 +16,51 @@ import NaturalIcon from '../../assets/images/growth.png'
 
 function Chapstick() {
   return (
-    <>
-      <section id="Chapstick">
-        <div className="chapstickSection">
-          <div class="split chapstickleft" >
-          <div className="containerTextTop">
-            <img className="naturalIcon" alt="chapstick icon" src={NaturalIcon} />
-            <Fade left>
-            <p className="detailsTop">100% Pure Organic, Raw & Unfiltered</p>
-            </Fade>
-            <Fade right>
-            <p className="detailsTop">From our hive, to your home.</p>
-            </Fade>
-            </div>
-            <div class="gallery borders">
-              <div class="clipped-border">
-                <img src={ChapstickOne} id="clipped" />
-              </div>
-              <div class="clipped-border">
-                <img src={ChapstickTwo} id="clipped" />
-              </div>
-              <div class="clipped-border">
-                <img src={ChapstickThree} id="clipped" />
-              </div>
-              <div class="clipped-border">
-                <img src={ChapstickFour} id="clipped" />
-              </div>
-              <div class="clipped-border">
-                <img src={ChapstickFive} id="clipped" />
-              </div>
-              <div class="shadow"></div>
-            </div>
-          </div>
-          <div class="split chapstickright">
-            <div class="centered">
-              <img className="bigIcon" alt="chapstick icon" src={ChapstickIcon} />
-              <Fade top big>
-                <h2 className="chapstickText">Chapstick</h2>
-              </Fade>
-              <p className="detailsP">When people find out that we are honey bee farming, they always ask about the honey. But bees also produce beeswax and something will need to be done with the beeswax when you harvest honey. We take this beeswax and filter out the impurities using a simple method of boiling in water and filtering. From there we just mix the beeswax with 1 parts beeswax and 2 parts coconut oil and a few drops of vitamin e. All the ingredients are organic.</p>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
+<>
+            <section id="Honey">
+                <div className="honeySection">
+                    <div class="split chapstickLeft" >
+                        <div className="containerTextTop">
+                            <img className="naturalIcon" alt="chapstick icon" src={NaturalIcon} />
+                            <Fade left>
+                            <p className="detailsTop">100% Pure Organic, Raw & Unfiltered</p>
+                            </Fade>
+                            <Fade right>
+                            <p className="detailsTop">From our hive, to your home.</p>
+                            </Fade>
+                        </div>
+                        <div class="gallery">
+                            <div class="clipped-border">
+                                <img src={ChapstickOne} id="clipped" />
+                            </div>
+                            <div class="clipped-border">
+                                <img src={ChapstickTwo} id="clipped" />
+                            </div>
+                            <div class="clipped-border">
+                                <img src={ChapstickThree} id="clipped" />
+                            </div>
+                            <div class="clipped-border">
+                                <img src={ChapstickFour} id="clipped" />
+                            </div>
+                            <div class="clipped-border">
+                                <img src={ChapstickFive} id="clipped" />
+                            </div>
+                            <div class="shadow"></div>
+                            {/* <button class="button-74" role="button"> 🍯 </button> */}
+                        </div>
+                    </div>
+                    <div class="split chapstickRight">
+                        <div class="centered">
+                            <img className="bigIcon" alt="chapstick icon" src={ChapstickIcon} />
+                            <Fade top big>
+                                <h2 className="chapstickText">Chapstick</h2>
+                            </Fade>
+                            <p className="detailsP"> When people find out that we are honey bee farming, they always ask about the honey. But bees also produce beeswax and something will need to be done with the beeswax when you harvest honey. We take this beeswax and filter out the impurities using a simple method of boiling in water and filtering. From there we just mix the beeswax with 1 parts beeswax and 2 parts coconut oil and a few drops of vitamin e. All the ingredients are organic.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
   );
 }
 
