f16990b2fb2be2fab29720f1c30d116b1919db01
William Gehrke <williamegehrke@gmail.com>
2023-01-03 17:20:34 -0500

    package.json homepage name

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -7,8 +7,8 @@ import Fade from 'react-reveal/Fade';
 function Hero() {
   return (
     <section id="Home">
-      <div style={{display: 'flex', justifyContent: 'right', alignItems: 'center'}} class="hero-image">
-        <div style={{width: '600px'}} className="hero-container">
+      <div style={{ display: 'flex', justifyContent: 'right', alignItems: 'center' }} class="hero-image">
+        <div style={{ width: '600px' }} className="hero-container">
           <Fade bottom>
             <h1 className="heroGerkeApiaries" >Gehrke Apiaries</h1>
           </Fade>
@@ -19,15 +19,13 @@ function Hero() {
             <Fade right>
               <p>From our hive, to your home.</p>
             </Fade>
-      
           </div>
           <Fade top>
-              <h1 className="heroHoneyChapstick" >Honey & Chapstick</h1>
-            </Fade>
+            <h1 className="heroHoneyChapstick" >Honey & Chapstick</h1>
+          </Fade>
         </div>
-        
       </div>
-      </section>
+    </section>
   );
 }
 
