34d7528bb89a9c8caaf1a56c01c8922b3cfe283c
William Gehrke <williamegehrke@gmail.com>
2023-01-09 17:45:52 -0500

    adjust styling to background

--- src/components/Chapstick/index.js
+++ src/components/Chapstick/index.js
@@ -17,9 +17,9 @@ import NaturalIcon from '../../assets/images/growth.png'
 function Chapstick() {
   return (
 <>
-            <section id="Honey">
+            <section id="Chapstick">
                 <div className="honeySection">
-                    <div class="split chapstickLeft" >
+                <div class="split honeyLeft" >
                         <div className="containerTextTop">
                             <img className="naturalIcon" alt="chapstick icon" src={NaturalIcon} />
                             <Fade left>
@@ -29,35 +29,34 @@ function Chapstick() {
                             <p className="detailsTop">From our hive, to your home.</p>
                             </Fade>
                         </div>
-                        <div className="galleryContainer">
+                        <div className="containerGallery">
                         <div class="gallery">
                             <div class="clipped-border">
-                                <img src={ChapstickOne} alt="chapstick" id="clipped" />
+                                <img src={ChapstickOne} alt="four oz honey" id="clipped" />
                             </div>
                             <div class="clipped-border">
-                                <img src={ChapstickTwo} alt="chapstick" id="clipped" />
+                                <img src={ChapstickTwo} alt="Honey Filtering" id="clipped" />
                             </div>
                             <div class="clipped-border">
-                                <img src={ChapstickThree} alt="chapstick" id="clipped" />
+                                <img src={ChapstickThree} alt="One ounce honey" id="clipped" />
                             </div>
                             <div class="clipped-border">
-                                <img src={ChapstickFour} alt="chapstick" id="clipped" />
+                                <img src={ChapstickFour} alt="Bottles of honey" id="clipped" />
                             </div>
                             <div class="clipped-border">
-                                <img src={ChapstickFive} alt="chapstick" id="clipped" />
+                                <img src={ChapstickFive} alt="Bottles of honey on table" id="clipped" />
                             </div>
                             <div class="shadow"></div>
-                            {/* <button class="button-74" role="button"> 🍯 </button> */}
                         </div>
                         </div>
                     </div>
-                    <div class="split chapstickRight">
+                    <div class="split honeyRight">
                         <div class="centered">
                             <img className="bigIcon" alt="chapstick icon" src={ChapstickIcon} />
                             <Fade top big>
                                 <h2 className="chapstickText">Chapstick</h2>
                             </Fade>
-                            <p className="detailsP"> When people find out that we are honey bee farming, they always ask about the honey. But bees also produce beeswax and something will need to be done with the beeswax when you harvest honey. We take this beeswax and filter out the impurities using a simple method of boiling in water and filtering. From there we just mix the beeswax with 1 parts beeswax and 2 parts coconut oil and a few drops of vitamin e. All the ingredients are organic.</p>
+                            <p className="detailsP">When people find out that we are honey bee farming, they always ask about the honey. But bees also produce beeswax and something will need to be done with the beeswax when you harvest honey. We take this beeswax and filter out the impurities using a simple method of boiling in water and filtering. From there we just mix the beeswax with 1 parts beeswax and 2 parts coconut oil and a few drops of vitamin e. All the ingredients are organic.</p>
                         </div>
                     </div>
                 </div>
