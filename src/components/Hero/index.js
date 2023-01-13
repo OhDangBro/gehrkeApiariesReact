e0b37d0e8afe2a293f20e84b2b15eaa60c2edd2b
William Gehrke <williamegehrke@gmail.com>
2023-01-13 12:35:05 -0500

    changed hero to video

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,31 +1,20 @@
 import React from "react";
 import Fade from 'react-reveal/Fade';
-
+import beeVideo from '../../assets/images/Bee.mp4'
 
 
 
 function Hero() {
   return (
-    <section id="Home">
-      <div style={{ display: 'flex', justifyContent: 'right', alignItems: 'center' }} class="hero-image">
-        <div style={{ width: '600px' }} className="hero-container">
-          <Fade bottom>
-            <h1 className="heroGerkeApiaries" >Gehrke Apiaries</h1>
-          </Fade>
-          <div className="heroSmallText">
-            <Fade left>
-              <p>Raw, organic, unfiltered & ethical.</p>
-            </Fade>
-            <Fade right>
-              <p>From our hive, to your home.</p>
-            </Fade>
-          </div>
-          <Fade top>
-            <h1 className="heroHoneyChapstick" >Honey & Chapstick</h1>
-          </Fade>
+   
+      <div class="heroContainer">
+        <video autoPlay muted src={beeVideo} loop="true"></video>
+        <div class="text-box">
+          <h1>GEHRKE</h1>
+          <h1 className="heroBottom">APIARIES</h1>
         </div>
       </div>
-    </section>
+   
   );
 }
 
