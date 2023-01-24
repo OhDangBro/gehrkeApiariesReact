eebdb9d9c7b2fb65ec04caadf208fd6bc0c65d28
William Gehrke <williamegehrke@gmail.com>
2023-01-24 17:30:53 -0500

    done i thinkgit add .

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Fade from 'react-reveal/Fade';
 import beeVideo from '../../assets/images/Bee.mp4'
 import beeGif from '../../assets/images/BeeGif.gif'
 
@@ -11,6 +10,7 @@ function Hero() {
     <video className="onDesktop" autoPlay muted src={beeVideo} alt="blue smoke" loop="true"></video>
     <img className="onMobile" autoPlay muted src={beeGif} alt="blue smoke" loop="true"></img>
     <div class="text-box">
+    <span class="dot"></span>
       <h1 className="heroBigText">GEHRKE</h1>
       <h1 className="heroBottom">APIARIES</h1>
 
